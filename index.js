a6cad57126c27d2be2469fa75a2901fa0a7107e9
DoaaAli <hanaahamdy840@gmail.com>
2024-11-14 04:19:50 +0200

    Porfoilo

---
+++ index.js
@@ -0,0 +1,11 @@
+
+const handlerTwo=()=>{
+    open("https://github.com/DoaaAliYounis/restaurant.git")
+
+}
+const handlerThree=()=>{
+    open("https://github.com/DoaaAliYounis/movielist.git")
+}
+const handlerFour=()=>{
+    open("./section-four/index.html")
+}
